07cabc0c76a7ada786822d14cc03113f21599464
Geoff Golda <gcgolda@gmail.com>
2019-02-13 01:03:00 -0700

    made FigureModel a data class instead of just class

--- app/src/main/java/ca/ggolda/guessayear/data/FigureModel.kt
+++ app/src/main/java/ca/ggolda/guessayear/data/FigureModel.kt
@@ -1,4 +1,4 @@
 package ca.ggolda.guessayear.data
 
-class FigureModel(var id: String, var name: String, var imgSrc: String, var figureDescription: String,
-                  var birthYr: Int, var deathYr: Int, var birthError: Int, var deathError: Int) {}
+data class FigureModel(var id: String, var name: String, var imgSrc: String, var figureDescription: String,
+                  var birthYr: Int, var deathYr: Int, var birthError: Int, var deathError: Int)
